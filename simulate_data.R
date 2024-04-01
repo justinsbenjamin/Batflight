9e518724143f144bc75adb101078b88b123dcb8c
Ben Bolker <bbolker@gmail.com>
2024-04-01 13:31:12 -0400

    BMB tweaks

--- simulate_data.R
+++ simulate_data.R
@@ -4,20 +4,36 @@ library(purrr)
 library(broom.mixed)
 library(tidyverse)
 
+if (packageVersion("broom.mixed") < "0.2.9.5") {
+    warning("please install the latest version of broom.mixed via remotes::install_github('bbolker/broom.mixed')")
+}
+
 #example from lecture, but for mass----
 set.seed(101)
 
+## BMB: using variable names like β is cute but may not be completely portable
 nGroup=15; days=seq(3, 60, by=3)
-β0=30; β_treat=(-5/60); β_day=(-0.04)
-sdint=5; sdslope=(1/60); corr=((-0.75) / (sqrt(1 + (-0.75^2)))); sdres=5
+
+## BMB: this matches what we talked about better
+β0=30; β_day=(-5/60); β_daytreat=(-1/60)
+
+form0 <- ~ 1 + day + treatment:day + (day | batID)
+form1 <- mass ~ 1 + day + treatment:day + (day | batID)
+
+corr_trans <- function(rho) rho/(sqrt(1+rho^2))
+sdint=5; sdslope=(1/60); corr=corr_trans(-0.75); sdres=5
 nBats <- nGroup*2
 
 
 ## JD: Think about using treatment*day instead; what are the units of that β??
-  #RS: ¯\_(ツ)_/¯
+## RS: ¯\_(ツ)_/¯
+## BMB: I think these comments may be getting out of date. 1 + day + treatment:day
+##      should be good as long as we are happy to assume no difference in weight on day 0
+##      (which is reasonable if you are assigning bats to treatments randomly)
 ## JD: These ...'s aren't the best practice; should figure out more about map
+## BMB: get JD to explain this comment, I don't understand it out of context
 
-sim <- function(nGroup, days, β0, β_treat, β_day
+sim <- function(nGroup, days, β0, β_day, β_daytreat
                 , sdint, sdslope, corr, sdres, ...
 ){
   batID <- as.factor(1:nBats) ## JD: Make this look less like a number with paste()
@@ -30,14 +46,14 @@ sim <- function(nGroup, days, β0, β_treat, β_day
   
   bat_data <- full_join(dat1, tdat, by="batID")
   
-  mass_data <- simulate_new( ~ 1  + day + treatment:day, 
+  mass_data <- simulate_new( form0,
                    nsim = 1,
                    family = "gaussian",
                    newdata = bat_data,
                    newparams = list(
-                     beta = c(β0, β_treat, β_day)  #when I change this from 0, the treatment group increases in mass? when it's set at 0, it also looks crazy 
-                    , theta = c(log(sdint), log(sdslope), corr)
-                    , betad = log(sdres) #making this smaller makes variation smaller
+                       beta = c(β0, β_day, β_daytreat)  #when I change this from 0, the treatment group increases in mass? when it's set at 0, it also looks crazy
+                     , theta = c(log(sdint), log(sdslope), corr)
+                     , betad = log(sdres) #making this smaller makes variation smaller
       )
     )
 
@@ -47,7 +63,7 @@ sim <- function(nGroup, days, β0, β_treat, β_day
     
 
 fit <- function(bat_data){
-  return(glmmTMB(mass ~  1  + day + treatment:day
+  return(glmmTMB(form1
                  , data=bat_data
                  , family = "gaussian"
   ))
@@ -56,52 +72,44 @@ fit <- function(bat_data){
 simCIs <- function(simfun, fitfun, ...){
   dat <- simfun(...)
   fit <- fitfun(dat)
-  c <- as.data.frame(confint(fit))
-  return(data.frame(vname=rownames(c)
-                    , est = c$Est
-                    , low = c$"2.5"
-                    , high = c$"97.5"
-  ))
+  tt <- (tidy(fit, effects = "fixed", conf.int = TRUE)
+      |> select(term, est = estimate, lwr = conf.low, upr = conf.high)
+      |> mutate(across(where(is.character), factor))
+  )
+  return(tt)
 }
 
   
 nReps <- 100 #made smaller for now
 
 print(simCIs(simfun=sim, fitfun=fit
-             , nGroup = 15, days=seq(3, 60, by=3)
-             , β0=30, β_treat=(-5/60), β_day=(-0.04)
-             , sdint=5, sdslope=(1/60), corr=((-0.75) / (sqrt(1 + (-0.75^2)))), sdres=5
-))
+           , nGroup = 15, days=seq(3, 60, by=3)
+             ## reuse values from above
+           , β0=β0, β_day=β_day, β_daytreat=β_daytreat
 
+           , sdint=sdint, sdslope=sdslope, corr=corr, sdres=5
+))
 
 
 cis <- map_dfr(1:nReps, simCIs, .id="sim", simfun=sim, fitfun=fit
                , nGroup = 15, days=seq(3, 60, by=3)
                , β0=30, β_treat=(-5/60), β_day=(-0.04)
-               , sdint=5, sdslope=(1/60), corr=((-0.75) / (sqrt(1 + (-0.75^2)))), sdres=5
+               , sdint=sdint, sdslope=sdslope, corr=corr, sdres=5
 )
 
 summary(cis)
-summary(cis |> mutate_if(is.character, as.factor))
-
-treatmentTrue <- 10 #what is this
+## BMB: *simulated* (true) value of the parameter we're testing
+treatmentTrue <- β_daytreat
 
 print(cis
       |> filter(vname=="day:treatmentexercise")
       |> summarize(
-        toohigh=mean(low>treatmentTrue)
-        , toolow=mean(high<treatmentTrue)
-        , ci_width=mean(high-low)
+             toohigh=mean(low>treatmentTrue)
+           , toolow=mean(high<treatmentTrue)
+           , ci_width=mean(high-low)
         , power = mean(low>0)
       )
 )
-
-#ahhh
-# Warning messages:
-#   1: In make_pars(c(beta = 1, beta = 1, beta = 1, betad = 0 ... :
-#                       unmatched parameter names: theta
-#  
-
 
 
 #For original sim data:
