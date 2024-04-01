c1715d3a6f28d17771845c6bf7073970df2d77dd
Ben Bolker <bbolker@gmail.com>
2024-04-01 17:24:08 -0400

    sims tweaking

--- simulate_data.R
+++ simulate_data.R
@@ -12,7 +12,8 @@ if (packageVersion("broom.mixed") < "0.2.9.5") {
 set.seed(101)
 
 ## BMB: using variable names like β is cute but may not be completely portable
-nGroup=15; days=seq(3, 60, by=3)
+n_per_group=15; days=seq(3, 60, by=3)
+n_groups <- 2
 
 ## BMB: this matches what we talked about better
 β0=30; β_day=(-5/60); β_daytreat=(-1/60)
@@ -21,8 +22,8 @@ form0 <- ~ 1 + day + treatment:day + (day | batID)
 form1 <- mass ~ 1 + day + treatment:day + (day | batID)
 
 corr_trans <- function(rho) rho/(sqrt(1+rho^2))
-sdint=5; sdslope=(1/60); corr=corr_trans(-0.75); sdres=5
-nBats <- nGroup*2
+sdint=5; sdslope=(1/60); corr=corr_trans(-0.75); sdres=1
+n_bats <- n_per_group*n_groups
 
 
 ## BMB: I think these comments may be getting out of date. 1 + day + treatment:day
@@ -32,19 +33,14 @@ nBats <- nGroup*2
 ## JD: These ...'s aren't the best practice; should figure out more about map
 ## BMB: get JD to explain this comment, I don't understand it out of context
 
-sim <- function(nGroup, days, β0, β_day, β_daytreat
+sim <- function(n_per_group, days, β0, β_day, β_daytreat
                 , sdint, sdslope, corr, sdres, ...
 ){
-  batID <- as.factor(1:nBats) ## JD: Make this look less like a number with paste()
-  
-  treatment <- as.factor(rep(c("control", "exercise"), each=nGroup))
-
+  batID <- as.factor(1:n_bats) ## JD: Make this look less like a number with paste()
+  treatment <- as.factor(rep(c("control", "exercise"), each=n_per_group))
   tdat <- data.frame(batID, treatment)
-  
   dat1 <- expand.grid(batID=batID, day=days)
-  
   bat_data <- full_join(dat1, tdat, by="batID")
-  
   mass_data <- simulate_new( form0,
                    nsim = 1,
                    family = "gaussian",
@@ -61,6 +57,18 @@ sim <- function(nGroup, days, β0, β_day, β_daytreat
 }
     
 
+s1 <- sim(n_per_group = 15, days=seq(3, 60, by=3)
+  , β0=β0, β_day=β_day, β_daytreat=β_daytreat
+  , sdint=sdint, sdslope=sdslope, corr=corr, sdres=sdres)
+
+plot_sim <- function(bat_data) {
+    ggplot(bat_data, aes(day, mass, colour = treatment)) +
+        geom_line(aes(group=batID))
+}
+plot_sim(s1)
+## BMB: you should **stop here** until you get parameter values
+## that make the simulated output look reasonable!
+
 fit <- function(bat_data){
   return(glmmTMB(form1
                  , data=bat_data
@@ -68,6 +76,8 @@ fit <- function(bat_data){
   ))
 }
 
+## BMB: could speed up by combining simfun and fitfun, using
+## nsim = nsim ...
 simCIs <- function(simfun, fitfun, ...){
   dat <- simfun(...)
   fit <- fitfun(dat)
@@ -78,39 +88,55 @@ simCIs <- function(simfun, fitfun, ...){
   return(tt)
 }
 
-  
-nReps <- 100 #made smaller for now
-
+## test once before running a lot
 print(simCIs(simfun=sim, fitfun=fit
-           , nGroup = 15, days=seq(3, 60, by=3)
+           , n_per_group = 15, days=seq(3, 60, by=3)
              ## reuse values from above
            , β0=β0, β_day=β_day, β_daytreat=β_daytreat
 
-           , sdint=sdint, sdslope=sdslope, corr=corr, sdres=5
+           , sdint=sdint, sdslope=sdslope, corr=corr, sdres=sdres
 ))
 
+  
+nReps <- 1000 ## made smaller for now
 
-cis <- map_dfr(1:nReps, simCIs, .id="sim", simfun=sim, fitfun=fit
-               , nGroup = 15, days=seq(3, 60, by=3)
-               , β0=30, β_treat=(-5/60), β_day=(-0.04)
-               , sdint=sdint, sdslope=sdslope, corr=corr, sdres=5
+## BMB: for slow stuff it is probably better to take apart the pieces
+## (1) generate a big list of fitted objects
+## (2) then run a separate workflow step to compute the summary
+##     statistics you're interested in
+
+
+system.time(
+    cis <- map_dfr(1:nReps, simCIs, .id="sim"
+                 , .progress = interactive()
+                 , simfun=sim, fitfun=fit
+                 , n_per_group = 15, days=seq(3, 60, by=3)
+                 , β0=30, β_treat=(-5/60), β_day=(-0.04)
+                 , sdint=sdint, sdslope=sdslope, corr=corr, sdres=sdres
+                   )
 )
+## BMB: about 6 minutes for 1000 reps on my laptop
+## (could make faster by parallelizing etc., but adds complexity)
 
+saveRDS(cis, "bat_mass_sims.RDS")
+
+## BMB: the rest of the code should go in a separate file so we can
+## split the slow and less-slow parts (i.e. cis <- readRDS("bat_mass_sims.RDS")
 summary(cis)
 ## BMB: *simulated* (true) value of the parameter we're testing
 treatmentTrue <- β_daytreat
+dt_cis <- (cis
+      |> filter(term=="day:treatmentexercise")
+    |> drop_na()
+)
 
-print(cis
-      %>% filter(term=="day:treatmentexercise")
-      %>% summarize(
+dt_cis |> summarize(
             toohigh=mean(lwr>treatmentTrue)
           , toolow=mean(upr<treatmentTrue)
           , ci_width=mean(upr-lwr)
           , power = mean(lwr>0)
-      )
-)
+          )
 
-saveRDS(cis, "bat_mass.RDS")
 
 #For original sim data:
 # ggplot(bat_data, aes(x = day, y = mass, colour = batID))  +
