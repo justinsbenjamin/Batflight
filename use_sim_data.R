d5f70047010a33b01d717648e56eef5214c0feed
rensol1 <renata@cogeco.ca>
2024-04-02 10:19:24 -0400

    moved from simulate_data.R

---
+++ use_sim_data.R
@@ -0,0 +1,22 @@
+library(tidyverse)
+
+#file created by "stimulate_data.R"
+cis <- readRDS("bat_mass_sims.RDS")
+β_daytreat=(-1/60)
+
+## BMB: the rest of the code should go in a separate file so we can
+## split the slow and less-slow parts (i.e. cis <- readRDS("bat_mass_sims.RDS")
+summary(cis)
+## BMB: *simulated* (true) value of the parameter we're testing
+treatmentTrue <- β_daytreat
+dt_cis <- (cis
+           %>% filter(term=="day:treatmentexercise")
+           %>% drop_na()
+)
+
+dt_cis |> summarize(
+  toohigh=mean(lwr>treatmentTrue)
+  , toolow=mean(upr<treatmentTrue)
+  , ci_width=mean(upr-lwr)
+  , power = mean(lwr>0)
+)
