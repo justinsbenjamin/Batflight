baf668d938af71727830d05dc10a0eb4fe14028c
rensol1 <renata@cogeco.ca>
2024-04-02 19:35:10 -0400

    nothing important

--- use_sim_data.R
+++ use_sim_data.R
@@ -6,8 +6,6 @@ set.seed(101)
 cis <- readRDS("bat_mass_sims.RDS")
 β_daytreat=(-1/60)
 
-## BMB: the rest of the code should go in a separate file so we can
-## split the slow and less-slow parts (i.e. cis <- readRDS("bat_mass_sims.RDS")
 summary(cis)
 ## BMB: *simulated* (true) value of the parameter we're testing
 treatmentTrue <- β_daytreat
@@ -16,9 +14,11 @@ dt_cis <- (cis
            %>% drop_na()
 )
 
-dt_cis |> summarize(
+dt_cis %>% summarize(
   toohigh=mean(lwr>treatmentTrue)
   , toolow=mean(upr<treatmentTrue)
   , ci_width=mean(upr-lwr)
   , power = mean(lwr>0)
 )
+
+#hi
