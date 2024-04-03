4709f63689375059e0d641efd76c72e9953f9fc2
rensol1 <renata@cogeco.ca>
2024-04-03 15:32:31 -0400

    included flight data

--- use_sim_data.R
+++ use_sim_data.R
@@ -1,14 +1,18 @@
 library(tidyverse)
 library(dplyr)
 
-#file created by "simulate_data.R"
+#file created by "simulate_mass_data.R"
 mass <- readRDS("bat_mass_sims.RDS")
-β_daytreat=(-1.5/60)
+#file created by "sim_flight_data.R"
+flightTime <- readRDS("bat_flight_sims.RDS")
+
+#mass sim data----
+β_daytreat_mass=(-1.5/60)
 
 summary(mass)#In an earlier version there were some NAs for lwr and upr, but seems okay now
 
 ## BMB: *simulated* (true) value of the parameter we're testing
-treatmentTrue <- β_daytreat #average *additional* loss per day (relative to control) in treatment group
+treatmentTrue_mass <- β_daytreat_mass #average *additional* loss per day (relative to control) in treatment group
 
 dt_mass <- (mass
            %>% filter(term=="day:treatmentexercise") #retains only rows where the value in the column term is day:treatmentexercise
@@ -20,4 +24,25 @@ dt_mass %>% summarize(
   , toolow=mean(upr<treatmentTrue) #proportion of CIs where the upper bound is lower than treatmentTrue 
   , ci_width=mean(upr-lwr) #mean width of CIs
   , power = mean(upr<0) #proportion of CIs where the lower bound is greater than zero. We are only interested in “power” to detect the true effect direction
+)
+
+#flight sim data----
+flightTime <- readRDS("bat_flight_sims.RDS")
+β_daytreat_flightTime=(2/60)
+
+summary(flightTime)
+
+## BMB: *simulated* (true) value of the parameter we're testing
+treatmentTrue_flightTime <- β_daytreat_flightTime #average *additional* loss per day (relative to control) in treatment group
+
+dt_flight <- (flightTime
+              %>% filter(term=="day:treatmentexercise") #retains only rows where the value in the column term is day:treatmentexercise
+              %>% drop_na() #removes rows missing values
+)
+
+dt_flight %>% summarize(
+  toohigh=mean(lwr>treatmentTrue) #proportion of CIs where the lower bound is greater than treatmentTrue 
+  , toolow=mean(upr<treatmentTrue) #proportion of CIs where the upper bound is lower than treatmentTrue 
+  , ci_width=mean(upr-lwr) #mean width of CIs
+  , power = mean(lwr>0) #proportion of CIs where the upper bound is lower than zero. We are only interested in “power” to detect the true effect direction
 )
