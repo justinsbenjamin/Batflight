8a56d33b65ae952d22b2c1dd7690bd589b6457f1
rensol1 <renata@cogeco.ca>
2024-04-23 13:03:58 -0400

    linear models

--- mass_sims_without_random.R
+++ mass_sims_without_random.R
@@ -5,18 +5,18 @@ library(broom.mixed)
 library(tidyverse)
 library(ggplot2); theme_set(theme_bw())
 
-set.seed(303)
+set.seed(707)
 
 n_per_group=15; days=seq(0, 60, by=3) 
 n_groups <- 2
 
-beta0=2; beta_day=(0.5/60); beta_daytreat=(2/60)
+beta0=30; beta_day=(-1.5/60); beta_daytreat=(-3/60)
 
 
 form0 <- ~ 1 + day + treatment:day 
-form1 <- flightTime ~ 1 + day + treatment:day 
+form1 <- mass ~ 1 + day + treatment:day 
 
-sdres=0.1
+sdres=0.5
 
 n_bats <- n_per_group*n_groups
 
@@ -27,33 +27,33 @@ sim <- function(n_per_group, days, beta0, beta_day, beta_daytreat
   treatment <- as.factor(rep(c("control", "exercise"), each=n_per_group))
   tdat <- data.frame(batID, treatment) 
   dat1 <- expand.grid(batID=batID, day=days)
-  flight_data_nore <- full_join(dat1, tdat, by="batID") 
-  flightTime <- simulate_new(form0, 
+  mass_nore <- full_join(dat1, tdat, by="batID") 
+  mass <- simulate_new(form0, 
                              nsim = 1,
-                             newdata = flight_data_nore,
+                             newdata = mass_nore,
                              newparams = list(
                                beta = c(beta0, beta_day, beta_daytreat) 
                                , betad = log(sdres) 
                              )
   )
   
-  flight_data_nore$flightTime <- flightTime[[1]] 
-  return(flight_data_nore) 
+  mass_nore$mass <- mass[[1]] 
+  return(mass_nore) 
 }
 
 s1 <- sim(n_per_group=n_per_group, days=days
           , beta0=beta0, beta_day=beta_day, beta_daytreat=beta_daytreat
           , sdres=sdres)
 
-plot_sim <- function(flight_data_nore) {
-  ggplot(flight_data_nore, aes(day, flightTime, colour = treatment)) +
+plot_sim <- function(mass_nore) {
+  ggplot(mass_nore, aes(day, mass, colour = treatment)) +
     geom_line(aes(group=batID))
 }
 plot_sim(s1)
 
-fit <- function(flight_data_nore){
+fit <- function(mass_nore){
   return(lm(form1 
-                 , data=flight_data_nore
+            , data=mass_nore
   ))
 }
 
@@ -69,15 +69,15 @@ simCIslm <- function(simfun, fitfun, ...){
 
 
 print(simCIslm(simfun=sim, fitfun=fit
-             , n_per_group=n_per_group, days=days
-             , beta0=beta0, beta_day=beta_day, beta_daytreat=beta_daytreat
-             , sdres=sdres
+               , n_per_group=n_per_group, days=days
+               , beta0=beta0, beta_day=beta_day, beta_daytreat=beta_daytreat
+               , sdres=sdres
 ))
 
 nReps <- 4000
 
 system.time(
-  cis <- map_dfr(1:nReps, simCIslm, .id="sim_flight" 
+  cis <- map_dfr(1:nReps, simCIslm, .id="sim_mass" 
                  , .progress = interactive() 
                  , simfun=sim, fitfun=fit 
                  , n_per_group = n_per_group, days=days 
@@ -89,29 +89,29 @@ system.time(
 
 summary(cis)
 
-treatmentTrue_flightTime <- beta_daytreat 
+treatmentTrue_mass<- beta_daytreat 
 
-dt_flight <- (cis
+dt_mass <- (cis
               %>% filter(term=="day:treatmentexercise") 
               %>% drop_na() 
 )
 
-dt_flight %>% summarize(
-  toohigh=mean(lwr>treatmentTrue_flightTime) 
-  , toolow=mean(upr<treatmentTrue_flightTime) 
+dt_mass %>% summarize(
+  toohigh=mean(lwr>treatmentTrue_mass) 
+  , toolow=mean(upr<treatmentTrue_mass) 
   , ci_width=mean(upr-lwr) 
   , power = mean(lwr>0) 
 )
 
-dt_flight <- (dt_flight
-              %>% mutate(across(sim_flight, ~ reorder(factor(.), est)))
+dt_mass <- (dt_mass
+              %>% mutate(across(sim_mass, ~ reorder(factor(.), est)))
 )
 
 no_x_axis <- theme(axis.title.x=element_blank(),
                    axis.text.x=element_blank(),
                    axis.ticks.x=element_blank())
 
-gg1 <- ggplot(dt_flight, aes(sim_flight, est)) +
+gg1 <- ggplot(dt_mass, aes(sim_mass, est)) +
   geom_pointrange(aes(ymin = lwr, ymax = upr)) +
   no_x_axis +
   geom_hline(yintercept = beta_daytreat,
@@ -122,5 +122,4 @@ gg1 <- ggplot(dt_flight, aes(sim_flight, est)) +
 
 print(gg1)
 
-ggsave("flighttime_CIs_no_random.png", plot = gg1, width = 6, height = 4, dpi = 300)
-
+#ggsave("flighttime_CIs_no_random.png", plot = gg1, width = 6, height = 4, dpi = 300)
